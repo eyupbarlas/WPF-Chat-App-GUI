3ed3a3693419bbb7606688a89075e0ede427b6e3
Bzzman <eyupbarlas2134@gmail.com>
2022-05-04 16:22:02 +0200

    Initial Commit

---
+++ ChatAppGUIDesignHW/MWM/ViewModel/MainViewModel.cs
@@ -0,0 +1,112 @@
+﻿using ChatAppGUIDesignHW.Core;
+using ChatAppGUIDesignHW.MWM.Model;
+using System;
+using System.Collections.ObjectModel;
+
+namespace ChatAppGUIDesignHW.MWM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        /* Commands */
+        public RelayCommand SendCommand { get; set; }
+
+        private ContactModel _selectedContact;
+
+        public ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set {
+                _selectedContact = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _message;
+        public string Message { 
+            get { return _message; }
+            set {
+                _message = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    FirstMessage = false
+                });
+
+                Message = "";
+
+            });
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Bzzman",
+                UsernameColor = "#409aff",
+                ImageSource = "https://pngimg.com/uploads/jester/jester_PNG12.png",
+                Message = "Testing1",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FirstMessage = true
+            });
+
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Bzzman",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://pngimg.com/uploads/jester/jester_PNG12.png",
+                    Message = "Testing",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = false,
+                    FirstMessage = false
+                });
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Bzzky",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://pngimg.com/uploads/jester/jester_PNG12.png",
+                    Message = "Testing",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = true
+                });
+            }
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Bzzman",
+                UsernameColor = "#409aff",
+                ImageSource = "https://pngimg.com/uploads/jester/jester_PNG12.png",
+                Message = "Last",
+                Time = DateTime.Now,
+                IsNativeOrigin = true
+            });
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"Contact {i}",
+                    ImageSource = "https://pngimg.com/uploads/gangster/gangster_PNG79.png",
+                    Messages = Messages
+                });
+            }
+        }
+    }
+}
---
+++ ChatAppGUIDesignHW/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace ChatAppGUIDesignHW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Dragging window logic
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        /// <summary>
+        /// Minimize window logic
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// Maximize & Normalize window logic
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WindowStateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+        }
+
+        /// <summary>
+        /// Close window logic
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
